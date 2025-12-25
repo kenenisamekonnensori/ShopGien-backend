cac4949646e92a94b8e3c240e9dbcae2e3812c08
Kenenisa Mekonnen <mekonnenkenenisa2@gmail.com>
2025-12-25 22:31:47 +0300

    added routes for register and login

---
+++ src/routes/auth.routh.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { login, register } from "../controllers/auth/auth.http.controller";
+
+
+const route = Router();
+
+route.post("/register", register);
+route.post("/login", login);
+
+export default route;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import searchRoutes from "./search.routes";
+import authRoutes from "./auth.routh";
 
 const router = Router();
 
 router.use("/search", searchRoutes);
+router.use("/auth", authRoutes);
 
 export default router;
